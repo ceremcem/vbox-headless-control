7b7bda8af5d75b35f2359b676175453b58bb281e
Cerem Cem ASLAN <cem@aktos.io>
2017-04-12 23:28:30 +0300

    fixed handling path with spaces

--- conf.sh
+++ conf.sh
@@ -3,8 +3,14 @@
 DIR=$(dirname "$(readlink -f "$0")")
 
 
-export vm_name=$(cat $DIR/../*.vbox | grep "<Machine" | grep -oP 'name="\K[^"]*')
-export SNAP_DIR=$DIR/../Snapshots
+export vm_name=$(grep "<Machine" "$DIR/../"*.vbox | grep -oP 'name="\K[^"]*')
+
+if [ "$vm_name" == "" ]; then 
+    echo "Can not find VM name!"
+    exit 1
+fi 
+
+export SNAP_DIR="$DIR/../Snapshots"
 
 echo "VM name is: $vm_name"
 
