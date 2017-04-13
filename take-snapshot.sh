096f9822504e08e133fe509a4bb2ecf81a3a3c63
Cerem Cem ASLAN <cem@aktos.io>
2017-04-13 03:03:11 +0300

    take-snapshot script is complete now

--- take-snapshot.sh
+++ take-snapshot.sh
@@ -5,17 +5,31 @@ DIR=$(dirname "$(readlink -f "$0")")
 
 SIZE_LIMIT=$1
 if [[ "$SIZE_LIMIT" == "" ]]; then 
-	echo "usage: $0 SIZE_LIMIT"
-	exit
+	SIZE_LIMIT=0
 fi
 
-curr_vdi_uuid=$(VBoxManage snapshot "aktos-1.6-backup" list --machinereadable | grep CurrentSnapshotUUID | awk -F= '{print $NF}') 
+## BACKUP: Seems this has no effect
+#curr_vdi_uuid=$(VBoxManage snapshot "$vm_name" list --machinereadable | grep CurrentSnapshotUUID | awk -F= '{print $NF}') 
+#echo "Curr snapshot's UUID: $curr_vdi_uuid"
+## END OF BACKUP 
 
-echo "Curr snapshot's UUID: $curr_vdi_uuid"
-VBoxManage snapshot $vm_name showvminfo $curr_vdi_uuid
-exit
+RW_FILE_SIZE=0
+while read -rd $'\0' vdi_file; do
+	if [ "$(VBoxManage showhdinfo $vdi_file | grep 'locked write')" != "" ]; then 
+		file_size=`du -m "$vdi_file" | cut -f1`
+		echo "VDI file: $(basename $vdi_file) ($file_size MB)"
+		RW_FILE_SIZE=$file_size
+	fi
+done < <( find $SNAP_DIR -iname "*.vdi" -print0 )
+
+if [[ $RW_FILE_SIZE > $SIZE_LIMIT ]]; then 
+	echo "Filesize exceeds given limit, taking snapshot..."
+else
+	echo "Filesize is smaller than given limit, not taking snapshot..."
+	exit 0
+fi
 
 DATE=`date +%Y%m%d.%H%M%S`
 
 echo "Taking snapshot of $vm_name with name $DATE"
-#/usr/bin/VBoxManage snapshot "$vm_name" take "$DATE" --description "Periodic snapshot by $0"
+/usr/bin/VBoxManage snapshot "$vm_name" take "$DATE" --description "Periodic snapshot by $0"
